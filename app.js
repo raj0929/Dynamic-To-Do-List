12361214f92487900a76e2d7e232ae9f41f1bcf2
raj0929 <142717255+raj0929@users.noreply.github.com>
2024-06-24 16:56:26 +0530

    Update

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB");
+mongoose.connect("mongodb+srv://testuser123:drnr35470@cluster0.admiwkf.mongodb.net/todolistDB");
 
 const itemsSchema = {
   name: String
